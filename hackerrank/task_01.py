3bccaff67f11228dbfb373092725f022ba121276
Rysbai <rysbai.muslim@gmail.com>
2019-01-12 20:17:51 +0600

    Changed task_02.

--- hackerrank/task_01.py
+++ hackerrank/task_01.py
@@ -1,6 +1,7 @@
 
 # The above code will print Hello, World! on your screen. 
 
+
 my_message = "Hello, World!"
 
 print(my_message)
--- hackerrank/task_02.py
+++ hackerrank/task_02.py
@@ -1,19 +1,23 @@
 
 # Task 
-# Given an integer, , perform the following conditional actions:
+# Given an integer, N, perform the following conditional actions:
 
 # If  is odd, print Weird
-# If  is even and in the inclusive range of  to , print Not Weird
-# If  is even and in the inclusive range of  to , print Weird
-# If  is even and greater than , print Not Weird
+# If  is even and in the inclusive range of 2 to 5, print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20, print Not Weird
+
 
 N = int(input())
 
 if N % 2 != 0:
     print("Weird")
-elif (N % 2 == 0) and (N >= 2) and (N < 6):
+
+elif (N % 2 == 0) and (N >= 2) and (N <= 5):
     print("Not Weird")
+
 elif (N % 2 == 0) and (N >= 6) and (N <= 20):
     print("Weird")
+
 elif (N % 2 == 0) and (N > 20):
     print("Not Weird")
