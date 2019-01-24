c8b7c7097e4850aff4b6b590738655a79706f01f
Rysbai <rysbai.coder@gmail.com>
2019-01-24 10:23:31 +0600

    Added tasks triangle but not completed

--- 2nd_week/hackerrank/medium/math/find_angle.py
+++ 2nd_week/hackerrank/medium/math/find_angle.py
@@ -1,3 +1,9 @@
 
 # TASK
 # https://www.hackerrank.com/challenges/find-angle/problem
+
+import math
+AB = int(input())
+BC = int(input())
+
+midlpoint = (math.sqrt(AB**2 + BC**2)) / 2
---
+++ 2nd_week/hackerrank/medium/math/triangle_quest_1.py
@@ -0,0 +1,21 @@
+# TASK
+# You are given a positive integer N. 
+# Print a numerical triangle of height N - 1 like the one below:
+
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+
+for i in range(1,int(input())): 
+    print(list(i*(10**power) for power in range(i-1, 0, -1)))
+
+
+# My output: 
+
+# []
+# [20]
+# [300, 30]
+# [4000, 400, 40]
