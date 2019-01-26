8c9322cd2eb7bb2cdf4d574415c6c5d7f8164dea
Rysbai <rysbai.coder@gmail.com>
2019-01-26 19:12:42 +0600

    Changed render_png

--- 3rd_week/matplt/render_png.py
+++ 3rd_week/matplt/render_png.py
@@ -1,29 +1,25 @@
 import pygal 
 import openpyxl
 
-wd = openpyxl.load_workbook('example.xlsx')
-sheet =  wd.get_sheet_by_name('Балыкчы шаары')
-x_labels = [sheet.cell(row=1, column=i).value for i in range(2, sheet.max_column + 1)]
-time = [sheet.cell(row=i, column=1).value for i in range(2, sheet.max_row + 1)]
-col_1 = [sheet.cell(row=i, column=2).value for i in range(2, sheet.max_row + 1)]
-col_2 = [sheet.cell(row=i, column=3).value for i in range(2, sheet.max_row + 1)]
-col_3 = [sheet.cell(row=i, column=4).value for i in range(2, sheet.max_row + 1)]
-col_4 = [sheet.cell(row=i, column=4).value for i in range(2, sheet.max_row + 1)]
-col_5 = [sheet.cell(row=i, column=6).value for i in range(2, sheet.max_row + 1)]
+wb = openpyxl.load_workbook('example.xlsx')
+sheets = wb.get_sheet_names()
+for sh in sheets:
+    sheet =  wb.get_sheet_by_name(sh)
+    print(sheet)
+    x_labels = [sheet.cell(row=1, column=i).value for i in range(2, sheet.max_column + 1)]
+    time = [sheet.cell(row=i, column=1).value for i in range(2, sheet.max_row + 1)]
 
-print(col_1)
-print(col_2)
-print(col_3)
-print(time)
-print(x_labels)
+    columns = []
+    for col in range(2, sheet.max_column + 1):
+        column = [sheet.cell(row=i, column=col).value for i in range(2, sheet.max_row + 1)]
+        columns.append(column)
 
+    print(columns)
+    hist = pygal.Bar()
+    hist.title = sheet.title
+    hist.x_labels = time
 
-hist = pygal.Bar()
-hist.title = sheet.title
-hist.x_labels = time
-hist.add(x_labels[0], col_1)
-hist.add(x_labels[1], col_2)
-hist.add(x_labels[2], col_3)
-hist.add(x_labels[3], col_4)
-hist.add(x_labels[4], col_5)
-hist.render_to_png('graph.png')
+    for i in range(len(columns)):
+        hist.add(x_labels[i], columns[i])
+
+    hist.render_to_png('graphs/{}.png'.format(sheet.title))
