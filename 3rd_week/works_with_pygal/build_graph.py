103893a2cec7fdf49e9e95b39862bd406c1d5485
Rysbai <rysbai.coder@gmail.com>
2019-01-28 13:14:20 +0600

    Changed all graphics :(

--- 3rd_week/works_with_pygal/build_graph.py
+++ 3rd_week/works_with_pygal/build_graph.py
@@ -6,22 +6,22 @@ filename = 'data.xlsx'
 wb = openpyxl.load_workbook(filename)
 sheets = wb.get_sheet_names()
 
+index = 1
 for sh in sheets:
     sheet =  wb.get_sheet_by_name(sh)
-    x_labels = [sheet.cell(row=1, column=i).value for i in range(2, sheet.max_column + 1)]
-    time = [sheet.cell(row=i, column=1).value for i in range(2, sheet.max_row + 1)]
 
-    columns = []
-    for col in range(2, sheet.max_column + 1):
-        column = [sheet.cell(row=i, column=col).value for i in range(2, sheet.max_row + 1)]
-        columns.append(column)
+    for row in range(2, sheet.max_row + 1):
+        x_labels = [sheet.cell(row=1, column=i).value for i in range(2, sheet.max_column + 1)]
+        y_labels = [sheet.cell(row=row, column=i).value for i in range(2, sheet.max_column + 1)]
+        time = sheet.cell(row=row, column=1).value
 
-    hist = pygal.Bar()
-    hist.title = sheet.title
-    hist.x_title = "Өлчөө убактысы"
-    hist.x_labels = time
+        hist = pygal.HorizontalBar()
+        hist.title = sheet.title
+        hist.x_title = 'Өлчөө убактысы: ' + time
+        hist.x_labels = x_labels
 
-    for i in range(len(columns)):
-        hist.add(x_labels[i], columns[i])
+        hist.add('(мг/м3)', y_labels)
+        file_name = str(index) + '_graph'
 
-    hist.render_to_png('graphs/{}.png'.format(sheet.title))
+        hist.render_to_png('graphs/{}.png'.format(file_name))
+        index += 1
--- 3rd_week/works_with_pygal/build_graphs2.py
+++ 3rd_week/works_with_pygal/build_graphs2.py
@@ -8,16 +8,16 @@ sheets = wb.get_sheet_names()
 sh_number = 1
 for sh in sheets:
     sheet =  wb.get_sheet_by_name(sh)
-    for col in range(1, sheet.max_column, 2):   
-        x_labels = [sheet.cell(row=i, column=col).value for i in range(2, sheet.max_row + 1) if i != None]
-        rows = [sheet.cell(row=i, column=col+1).value for i in range(2, sheet.max_row + 1) if i != None]
-        device = sheet.cell(row=1, column=col).value
+    x_labels = [sheet.cell(row=i, column=1).value for i in range(2, sheet.max_row - 2 + 1)]
+    # x_labels = [r'$\mathregular{CO_2}$', r'$\mathregular{H_2S}$', 'CO', r'$\mathregular{O_2}$']
+    y_labels = [sheet.cell(row=i, column=2).value for i in range(2, sheet.max_row - 2 + 1)]
+    t = sheet.cell(row=sheet.max_row - 1, column=2).value
+    h = sheet.cell(row=sheet.max_row, column=2).value
 
-        hist = pygal.Bar()
-        hist.title = sheet.title
-        hist.x_labels = x_labels
-        hist.add(device, rows)
-        device = device + str(sh_number)
-        hist.render_to_png('graphs2/{}.png'.format(device))
-        print(device)
+    hist = pygal.HorizontalBar()
+    hist.title = sheet.title
+    hist.x_title = "t = {}°  h = {}%".format(t, h)
+    hist.x_labels = x_labels
+    hist.add("(мг/м3)", y_labels)
+    hist.render_to_png('graphs2/{}.png'.format(sheet.title))
     sh_number +=1
